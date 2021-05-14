4e347dda0aaeff9dd727a15c056acf6457fd5ddd
kzinthant <kzinthant.d3v@gmail.com>
2021-05-14 19:44:28 +0630

    test

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-console': 'warn',
+  },
+};
